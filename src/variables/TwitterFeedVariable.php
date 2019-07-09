c41ee5d1fc46c1d0c6ec8cf85972f2f5203492fd
Chris Keefer <chriskeefer@mobyinc.com>
2019-07-09 16:17:10 -0700

    use GET request to retrieve tweets

--- src/variables/TwitterFeedVariable.php
+++ src/variables/TwitterFeedVariable.php
@@ -44,12 +44,8 @@ class TwitterFeedVariable
             Craft::info('Starting Twitter API query.', __METHOD__); 
             try {
                 $tweet = $this->instance->twitterService
-                ->buildOauth('https://api.twitter.com/1.1/statuses/user_timeline.jsoon', 'POST')
-                ->setPostfields([
-                    'screen_name' => $handle,
-                    'exclude_replies' => true,
-                    'count' => 1,
-                ])
+                ->setGetfield("?screen_name={$handle}")
+                ->buildOauth('https://api.twitter.com/1.1/statuses/user_timeline.jsoon', 'GET')
                 ->performRequest();
             } catch (Exception $e) {
                 Craft::warning('Twitter API query failed.', __METHOD__); 
